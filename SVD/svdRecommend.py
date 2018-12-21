3287279769406f9f08190dd1d2d9fa838b5f0734
724686158 <solitarius.holic@gmail.com>
2018-12-21 11:11:22 +0800

    Update svdRecommend.py

--- SVD/svdRecommend.py
+++ SVD/svdRecommend.py
@@ -57,50 +57,6 @@ def cosSim(inA, inB):
     denom = la.norm(inA)*la.norm(inB)
     return 0.5 + 0.5*(num/denom)
 
-# 基于物品相似度的推荐引擎
-def standEst(dataMat, user, simMeas, item):
-    """standEst(计算某用户未评分物品中，以对该物品和其他物品评分的用户的物品相似度，然后进行综合评分)
-    Args:
-        dataMat         训练数据集
-        user            用户编号
-        simMeas         相似度计算方法
-        item            未评分的物品编号
-    Returns:
-        ratSimTotal/simTotal     评分（0～5之间的值）
-    """
-    # 得到数据集中的物品数目
-    n = shape(dataMat)[1]
-    # 初始化两个评分值
-    simTotal = 0.0
-    ratSimTotal = 0.0
-    # 遍历行中的每个物品（对用户评过分的物品进行遍历，并将它与其他物品进行比较）
-    for j in range(n):
-        userRating = dataMat[user, j]
-        # 如果某个物品的评分值为0，则跳过这个物品
-        if userRating == 0:
-            continue
-        # 寻找两个用户都评级的物品
-        # 变量 overLap 给出的是两个物品当中已经被评分的那个元素的索引ID
-        # logical_and 计算x1和x2元素的真值。
-        overLap = nonzero(logical_and(dataMat[:, item].A > 0, dataMat[:, j].A > 0))[0]
-        # 如果相似度为0，则两着没有任何重合元素，终止本次循环
-        if len(overLap) == 0:
-            similarity = 0
-        # 如果存在重合的物品，则基于这些重合物重新计算相似度。
-        else:
-            similarity = simMeas(dataMat[overLap, item], dataMat[overLap, j])
-        # print('the %d and %d similarity is : %f'(iten,j,similarity))
-        # 相似度会不断累加，每次计算时还考虑相似度和当前用户评分的乘积
-        # similarity  用户相似度，   userRating 用户评分
-        simTotal += similarity
-        ratSimTotal += similarity * userRating
-    if simTotal == 0:
-        return 0
-    # 通过除以所有的评分总和，对上述相似度评分的乘积进行归一化，使得最后评分在0~5之间，这些评分用来对预测值进行排序
-    else:
-        return ratSimTotal/simTotal
-
-
 # 基于SVD的评分估计
 # 在recommend() 中，这个函数用于替换对standEst()的调用，该函数对给定用户给定物品构建了一个评分估计值
 def svdEst(dataMat, user, simMeas, item):
@@ -119,7 +75,7 @@ def svdEst(dataMat, user, simMeas, item):
     simTotal = 0.0
     ratSimTotal = 0.0
     # 奇异值分解
-    # 在SVD分解之后，我们只利用包含了90%能量值的奇异值，这些奇异值会以NumPy数组的形式得以保存
+    # 在SVD分解之后，只利用包含了90%能量值的奇异值
     U, Sigma, VT = la.svd(dataMat)
 
     # # 分析 Sigma 的长度取值
@@ -142,9 +98,7 @@ def svdEst(dataMat, user, simMeas, item):
         userRating = dataMat[user, j]
         if userRating == 0 or j == item:
             continue
-        # 相似度的计算方法也会作为一个参数传递给该函数
         similarity = simMeas(xformedItems[item, :].T, xformedItems[j, :].T)
-        # for 循环中加入了一条print语句，以便了解相似度计算的进展情况。如果觉得累赘，可以去掉
         print('the %d and %d similarity is: %f' % (item, j, similarity))
         # 对相似度不断累加求和
         simTotal += similarity
@@ -197,20 +151,13 @@ def analyse_data(Sigma, loopNum=20):
     SigmaSum = sum(Sig2)
     eyenum = 0
     for i in range(loopNum):
-        SigmaI = sum(Sig2[:i+1])
-        '''
-        根据自己的业务情况，就行处理，设置对应的 Singma 次数
-        通常保留矩阵 80% ～ 90% 的能量，就可以得到重要的特征并取出噪声。
-        '''
+        SigmaI = sum(Sig2[:i])
         # 保留90%
         if eyenum == 0 and (SigmaI/SigmaSum*100) >= 90:
-            eyenum = i+1
+            eyenum = i
     return eyenum
 
 
 
 if __name__ == "__main__":
-    # print(recommend(dataMat=mat(loadExData()), user=2, N=4, simMeas=cosSim, estMethod=svdEst))
     print(recommend(dataMat=mat(loadWebData()).T, user=4, N=2, simMeas=cosSim, estMethod=svdEst))
-    print(recommend(dataMat=mat(loadWebData()).T, user=4, N=2, simMeas=cosSim, estMethod=standEst))
-    # print(recommend(dataMat=mat(loadWebData()).T, user=4, N=3, simMeas=cosSim, estMethod=svdEst))
